b6aa697eff423333368c7a8822a388afeea726eb
Punlork <chekpunlork9999@gmail.com>
2023-10-03 23:39:17 +0700

    Update: rename app

--- android/app/src/main/java/com/jabhuy/app/MainActivity.java
+++ android/app/src/main/java/com/jabhuy/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativeapp;
+package com.jabhuy.app;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "reactNativeApp";
+    return "JabHuy";
   }
 
   /**
--- android/app/src/main/java/com/jabhuy/app/MainApplication.java
+++ android/app/src/main/java/com/jabhuy/app/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativeapp;
+package com.jabhuy.app;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
